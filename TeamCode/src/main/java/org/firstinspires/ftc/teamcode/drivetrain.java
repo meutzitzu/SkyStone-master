cd78b6b4b93bed533a3c77bc0be655325f631ab2
Rapeanu George <alexandrurapeanu@yahoo.com>
2019-10-28 14:20:08 +0200

    fixed initial commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -53,11 +24,15 @@ public class drivetrain extends LinearOpMode {
     public void runOpMode() {
 
         //Setup Hardware
-        driveFL  = hardwareMap.get(DcMotor.class, "FL");
-        driveFR  = hardwareMap.get(DcMotor.class, "FR");
-        driveRL  = hardwareMap.get(DcMotor.class, "RL");
-        driveRR  = hardwareMap.get(DcMotor.class, "RR");
+        driveFL = hardwareMap.get(DcMotor.class, "FL");
+        driveFR = hardwareMap.get(DcMotor.class, "FR");
+        driveRL = hardwareMap.get(DcMotor.class, "RL");
+        driveRR = hardwareMap.get(DcMotor.class, "RR");
 
+        driveFL.setDirection(DcMotor.Direction.FORWARD);
+        driveFR.setDirection(DcMotor.Direction.REVERSE);
+        driveRL.setDirection(DcMotor.Direction.FORWARD);
+        driveRR.setDirection(DcMotor.Direction.REVERSE);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -67,40 +42,62 @@ public class drivetrain extends LinearOpMode {
         double vX, vY, vR;     //robot speed as commanded by driver (can be in either LOCAL or GLOBAL coordinates, depending on the MODE)
 
         //CTRL
-        int MODE=0;
+        int MODE = 0;
                             /* MODE 0 means simple "first person" controls, no trig tricks needed,
-
                              MODE 1 means controlling the robot in GLOBAL coordinates
                              (left's always left, right's always right, regardless of robot orientation)(some trig required)
                              see MODE STATE MACHINE */
 
-        double LocalX, LocalY; //robot speed in local coordinates
-        double X, Y, A;
-        //OUTPUT
-        double  power_FL,   power_FR,
 
-                power_RL,   power_RR;
+        double LocalX = 0, LocalY = 0; //robot speed in local coordinates
+        double X=0, Y=0, A=0;
+        //OUTPUT
+        double  power_FL, power_FR,
+
+                power_RL, power_RR;
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             //READ INPUTS
             vY = -gamepad1.left_stick_y;
-            vX =  gamepad1.left_stick_x;
-            vR =  gamepad1.left_trigger - gamepad1.right_trigger;
+            vX = gamepad1.left_stick_x;
+            vR = gamepad1.right_trigger - gamepad1.left_trigger;
+
+            if (gamepad1.x&&gamepad1.y) {
+                if (gamepad1.dpad_right) {
+                    MODE = 0;
+                } else if (gamepad1.dpad_left){
+                    MODE = 1;
+                }
+            }
+
 
             /*
                 UPDATE X,Y,A from sensors
              */
-            A=0;
-            LocalY=vX*Math.sin(A)+vY*Math.cos(A);
-            LocalY=vX*Math.cos(A)-vY*Math.sin(A);
 
-                    // O D O M E T R Y goes HERE !!
 
-            power_FL    = Range.clip(+LocalX + LocalY + vR, -1.0, 1.0) ;
-            power_FR    = Range.clip(-LocalX + LocalY - vR, -1.0, 1.0) ;
-            power_RL    = Range.clip(-LocalX + LocalY + vR, -1.0, 1.0) ;
-            power_RR    = Range.clip(+LocalX + LocalY - vR, -1.0, 1.0) ;
+            switch (MODE) {
+                case 0: {
+                    LocalY = vY;
+                    LocalX = vX;
+                    break;
+                }
+
+                case 1: {
+                    A = Math.PI / 2;
+                    LocalY = vX * Math.sin(A) + vY * Math.cos(A);
+                    LocalY = vX * Math.cos(A) - vY * Math.sin(A);
+                    break;
+                }
+            }
+
+            // O D O M E T R Y goes HERE !!
+
+            power_FL = Range.clip(+LocalX + LocalY + vR, -1.0, 1.0);
+            power_FR = Range.clip(-LocalX + LocalY - vR, -1.0, 1.0);
+            power_RL = Range.clip(-LocalX + LocalY + vR, -1.0, 1.0);
+            power_RR = Range.clip(+LocalX + LocalY - vR, -1.0, 1.0);
 
 
             // Send calculated power to wheels
@@ -111,10 +108,13 @@ public class drivetrain extends LinearOpMode {
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("MODE", ": (%d)", MODE);
             telemetry.addData("IN", "Y (%.2f), X (%.2f)", vY, vX);
-            telemetry.addData("Motors", "FL (%.2f), FR (%.2f), RL (%.2f), RR (%.2f)",power_FL, power_FR, power_RL, power_RR);
+
+            telemetry.addData("Motors", "FL (%.2f), FR (%.2f), RL (%.2f), RR (%.2f)", power_FL, power_FR, power_RL, power_RR);
 
             telemetry.update();
+
         }
     }
 }
